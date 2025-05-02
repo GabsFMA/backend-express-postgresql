595871a695bea2f4aaa035059db30f183f9604e4
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-05-02 14:34:58 -0300

    fix: correcting requests

--- requests/GET_Protected_Valid_Token.sh
+++ requests/GET_Protected_Valid_Token.sh
@@ -1,3 +1,3 @@
 curl --request GET \
   --url 'http://localhost:3000/secureExampleRoute/protected' \
-  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTNjYzU4NDIwMWEwMjZlNDZkNDBjNyIsImlhdCI6MTc0NjEzNzA0OCwiZXhwIjoxNzQ2MTQwNjQ4fQ.YHa8x2z1gKDFsXSSWzVQX_DluJLPVyeu99nwIl2lU2M'
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNzQ2MjA2MjkzLCJleHAiOjE3NDYyMDk4OTN9.A6zYe8Ojflgf0XqwoqR79WLWg6m9KZ5l6M4mKmcV0-E'
--- requests/POST_Login_User_Error_Invalid_Password.sh
+++ requests/POST_Login_User_Error_Invalid_Password.sh
@@ -2,6 +2,6 @@ curl --request POST \
   --url 'http://localhost:3000/users/login' \
   --header 'Content-Type: application/json' \
   --data '{
-    "email": "newuser@example.com",
+    "email": "example@email.com",
     "password": "wrongpassword"
   }'
